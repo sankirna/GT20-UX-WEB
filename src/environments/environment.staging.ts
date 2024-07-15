0dc49bd5ac0fa2543fc61bfdae14148f62788628
DESKTOP-O2RGDVD\Rinku <sankirnarana@gmail.com>
2024-07-15 15:31:23 +0530

    no message

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  apiUrl: 'http://13.60.99.168/api/api/',
+  apiUrl: 'http://13.60.47.237/api/api/',
   production: true,
-  defaultUrl:'http://13.60.99.168/api/api/StaticFiles/Default_Image/no-image.jpg',
+  defaultUrl:'http://13.60.47.237/api/api/StaticFiles/Default_Image/no-image.jpg',
 };
